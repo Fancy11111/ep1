68e2f142bc2ed42bcb9be044d49f66bba18d982c
Daniel Fenz <dani.fenz@aon.at>
2020-10-31 15:11:40 +0100

    added solution for exercise 2

--- Aufgabenblatt2/src/Aufgabe2.java
+++ Aufgabenblatt2/src/Aufgabe2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
     Aufgabe 2) Verschachtelte Schleifen und Verzweigungen
 */
@@ -6,5 +8,71 @@ public class Aufgabe2 {
     public static void main(String[] args) {
 
         //TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        Scanner scanner = new Scanner(System.in);
+        int figHeight = 0;
+        boolean readInt = false;
+        System.out.print("Please enter an uneven positive that is <= 53: ");
+        while(!readInt && scanner.hasNext()) {
+            if(scanner.hasNextInt()) {
+                figHeight = scanner.nextInt();
+                if(figHeight % 2 == 1 && figHeight <= 53) {
+                    readInt = true;
+                }
+                else {
+                    System.out.print("Please enter an uneven positive that is <= 53: ");
+                }
+            }
+            else {
+                System.out.print("Please enter an uneven positive that is <= 53: ");
+                scanner.next();
+            }
+        }
+
+        if(!(figHeight % 2 == 1 && figHeight <= 53)) {
+            System.exit(1);
+        }
+        int spaces; // Spaces left and right of figure
+        int signs;
+        int width = figHeight + 1;
+        int yMiddle = (figHeight / 2);
+        int direction = -1;
+        for(int i = 0; i < yMiddle; i++) {
+            spaces = (width - (i * 2) - 2) / 2;
+            for(int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('/');
+            for(int j = 0; j < i*2; j++) {
+                System.out.print('A');
+            }
+            System.out.print('\\');
+            for(int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+
+        System.out.print('|');
+        for(int i = 0; i < width - 2; i++) {
+            System.out.print('A');
+        }
+        System.out.println('|');
+
+        for(int i = yMiddle - 1; i > -1; i--) {
+            spaces = (width - (i * 2) - 2) / 2;
+            for(int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            System.out.print('\\');
+            for(int j = 0; j < i*2; j++) {
+                System.out.print('A');
+            }
+            System.out.print('/');
+            for(int j = 0; j < spaces; j++) {
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+
     }
 }
