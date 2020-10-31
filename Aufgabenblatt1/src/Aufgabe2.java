24880d9f2c3640b8ce67e4f9cf2ce058cb138b06
Daniel Fenz <dani.fenz@aon.at>
2020-10-31 12:55:10 +0100

    finished exercise 2

--- Aufgabenblatt1/src/Aufgabe2.java
+++ Aufgabenblatt1/src/Aufgabe2.java
@@ -4,8 +4,50 @@
 public class Aufgabe2 {
     
     public static void main(String[] args) {
-        
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        //a
+        int sum = 0;
+        for(int i = 10; i < 110; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+        System.out.println();
+
+        //b
+        for(int i = 13; i < 121; i++) {
+            if(i % 4 == 0 &&  i % 6 == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+
+        //c
+        for(int i = 169; i > 34; i--) {
+            if(i % 17 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\n");
+
+        //d
+        int product = 1;
+        for(int i = -10; i < 11; i++) {
+            if(i % 2 == 1 || i % 2 == -1) {
+                product *= i;
+            }
+        }
+        System.out.println(product);
+        System.out.println();
+
+        //e
+        String text = "Schiffe für den Fischfang.";
+        int occurences = 0;
+        for(int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == 'f'){
+                occurences++;
+            }
+        }
+        System.out.println(occurences);
+
     }
 }
 
